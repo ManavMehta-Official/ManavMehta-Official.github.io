5f4fd38a652aaf38123c574430ab7776667134c9
ManavMehta-Official <manavmehta.official@gmail.com>
2021-08-26 18:31:09 +0530

    updated

--- main.js
+++ main.js
@@ -1,37 +1,20 @@
-var toastTrigger = document.getElementById('liveToastBtn')
-var toastLiveExample = document.getElementById('liveToast')
-
-
-
-if (toastTrigger == true) {
-toastTrigger.addEventListener('click', function () {
-    var toast = new bootstrap.Toast(toastLiveExample)
-    toast.show()
-}) 
-if (toastTrigger == false) {
-    toastLiveExample.style.display = 'none';
-}
-}
-
-
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+var exampleEl = document.getElementById('example')
+var popover = new bootstrap.Popover(exampleEl, options)
 
 
 function copyToClipboard(element) {
-    var $temp = $("<input>");
-    $("body").append($temp);
-    $temp.val($(element).text()).select();
-    document.execCommand("copy");
-    $temp.remove();
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
 }
 
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
 })
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel-mat');
-    var instances = M.Carousel.init(elems, options);
-  });
+
