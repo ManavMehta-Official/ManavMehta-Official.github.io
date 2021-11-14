ccfec83c3c9291fd8b1e39c5d8d21e04b9134025
ManavMehta-Official <manavmehta.official@gmail.com>
2021-11-14 20:10:57 +0530

    updated

--- main.js
+++ main.js
@@ -4,13 +4,6 @@ var exampleEl = document.getElementById('example')
 var popover = new bootstrap.Popover(exampleEl, options)
 
 
-function copyToClipboard(element) {
-  var $temp = $("<input>");
-  $("body").append($temp);
-  $temp.val($(element).text()).select();
-  document.execCommand("copy");
-  $temp.remove();
-}
 
 
 
